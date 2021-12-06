73c09b8d656bffb0e6ecb3562da2cabca124e307
virinarl <vrodriguezl@fi.uba.ar>
2021-12-06 00:03:52 -0300

    Functional News Feed

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const NewsFeed = () => {
-  return <div className="news-feed">News Feed</div>;
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://crypto-news-live.p.rapidapi.com/news",
+      headers: {
+        "x-rapidapi-host": "crypto-news-live.p.rapidapi.com",
+        "x-rapidapi-key": "45afa4811dmsh948a203d5fc2f2bp1944f2jsn85b5e2f5a078",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        setArticles(response.data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
+
+  const firstSevenArticles = articles?.slice(0, 7);
+
+  return (
+    <div className="news-feed">
+      <h2>News Feed</h2>
+      {firstSevenArticles?.map((article, _index) => (
+        <div key={_index}>
+          <a href={article.url}>
+            <p>{article.title}</p>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default NewsFeed;
