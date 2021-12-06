2f7627ca7b6bacf0af1aa514c892f7bcab34f98f
virinarl <vrodriguezl@fi.uba.ar>
2021-12-05 22:39:16 -0300

    Show info in exchange rate component

--- src/components/ExchangeRateDisplay.js
+++ src/components/ExchangeRateDisplay.js
@@ -1,5 +1,17 @@
-const ExchangeRateDisplay = () => {
-  return <div className="exchange-rate">Exchange Rate</div>;
+const ExchangeRateDisplay = ({
+  exchangeRate,
+  primaryCurrency,
+  secondaryCurrency,
+}) => {
+  return (
+    <div className="exchange-rate">
+      <h3>Exchange Rate</h3>
+      <p>{exchangeRate}</p>
+      <p>
+        {primaryCurrency} to {secondaryCurrency}
+      </p>
+    </div>
+  );
 };
 
 export default ExchangeRateDisplay;
